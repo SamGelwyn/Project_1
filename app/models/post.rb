57fbdf7fc8b2914518cd2815fa78f10cbb57dd4d
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-04-30 10:17:21 +1000

    following users interface

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user, :optional => true,
-  default_scope -> { order(created_at: :desc) }
+  belongs_to :user, :optional => true
+  default_scope { order(created_at: :desc) }
 
   validates :content, presence: true, length: { minimum: 5 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
-  # has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent:    :destroy
-  # has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent:   :destroy
-  # has_many :following, through: :active_relationships, source: :followed
-  # has_many :followers, through: :passive_relationships, source: :follower
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent:    :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
@@ -15,19 +15,19 @@ class User < ApplicationRecord
     # Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 
-  # # Follows a user.
-  # def follow(other_user)
-  #   active_relationships.create(followed_id: other_user.id)
-  # end
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
 
-  # # Unfollows a user.
-  # def unfollow(other_user)
-  #   active_relationships.find_by(followed_id: other_user.id).destroy
-  # end
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 
-  # # Returns true if the current user is following the other user.
-  # def following?(other_user)
-  #   following.include?(other_user)
-  # end
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
   
 end
